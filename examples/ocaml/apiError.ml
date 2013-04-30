fe1bcfbd75a39d96d91ac15397651ac9d7124ae7
db0company <db0company@gmail.com>
2013-04-30 17:26:38 +0200

    [OCaml] Client-side error handling

--- examples/ocaml/apiError.ml
+++ examples/ocaml/apiError.ml
@@ -26,3 +26,21 @@ let success =
     stype   = "Success";
     code    = 0;
   }
+
+(* ************************************************************************** *)
+(* Client-side errors                                                         *)
+(* ************************************************************************** *)
+
+let invalid_json =
+  {
+    message = "The JSON tree response is not formatted as expected.";
+    stype   = "CLIENT_InvalidJSON";
+    code    = -18;
+  }
+
+let requirement_missing =
+  {
+    message = "One requirement is missing";
+    stype   = "CLIENT_RequirementMissing";
+    code    = -25;
+  }
