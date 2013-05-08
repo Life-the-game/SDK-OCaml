fd0b172d3620c1760215a2418227fbd6a682e4fb
db0company <db0company@gmail.com>
2013-05-08 07:19:04 +0200

    [OCaml] API user's conversations methods

---
+++ examples/ocaml/apiConversation.mli
@@ -0,0 +1,38 @@
+(* ************************************************************************** *)
+(* Project: La Vie Est Un Jeu - Public API, example with OCaml                *)
+(* Description: tools to get/edit achievements                                *)
+(* Author: db0 (db0company@gmail.com, http://db0.fr/)                         *)
+(* Latest Version is on GitHub: https://github.com/LaVieEstUnJeu/Public-API   *)
+(* ************************************************************************** *)
+
+open ApiTypes
+
+(* ************************************************************************** *)
+(** Types                                                                     *)
+(* ************************************************************************** *)
+
+type message =
+    {
+      sender_ref : int;
+      content    : string;
+    }
+
+type t =
+    {
+      info             : Info.t;
+      referenced_users : ApiUser.t list;
+      messages         : message List.t;
+    }
+
+(* ************************************************************************** *)
+(** Api Methods                                                               *)
+(* ************************************************************************** *)
+
+(** Get the conversation                                                      *)
+val get : auth:auth -> ?index:int option -> ?limit:int option -> id -> t Api.t
+
+(** Post a message in the conversation                                        *)
+val post : auth:auth -> string -> id -> message Api.t
+
+(** Delete a message in the conversation                                      *)
+val delete : auth:auth -> id_message:id -> id -> unit Api.t
