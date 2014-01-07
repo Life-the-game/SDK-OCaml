a19a7d9dd21b903e1c316373e1f83c0645c7764e
db0company <db0company@gmail.com>
2014-01-07 15:22:00 +0100

    New prod URL

--- apiConf.ml
+++ apiConf.ml
@@ -6,7 +6,7 @@
 (** Configuration of the library                                              *)
 
 (** The URL of the API Web service                                            *)
-let base_url = ref "http://api.glife.fr"
+let base_url = ref "http://api.life.tl/api/v1"
 
 (** Print log messages or not?                                                *)
 let verbose = ref false
